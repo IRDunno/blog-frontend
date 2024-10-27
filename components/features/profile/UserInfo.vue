708ebcc43470ff19e09951876485e83003e70117
Aby Calago <calagoaby@gmail.com>
2024-10-27 20:05:18 +0800

    feat: add profile image

--- components/features/profile/UserInfo.vue
+++ components/features/profile/UserInfo.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-    <div class="user-info">
-      <span>Name</span>
-      <p class="fw-bold">{{ user.name }}</p>
-      <span>Email</span>
-      <p class="fw-bold">{{ user.email }}</p>
+    <div class="row gy-3">
+      <div class="col-lg-4 text-center">
+        <img :src="user.image" class="rounded-circle" height="100px" alt="">
+        <hr class="d-md-block d-lg-none">
+      </div>
+      <div class="col-lg-8">
+        <span>Name</span>
+        <p class="fw-bold">{{ user.name }}</p>
+        <span>Email</span>
+        <p class="fw-bold">{{ user.email }}</p>
+      </div>
     </div>
-    <hr>
+    <hr />
     <div class="update-buttons d-flex gap-1">
-      <button @click="editPassword" class="btn btn-outline-warning btn-sm">Update password</button>
-      <button @click="editInfo" class="btn btn-outline-primary btn-sm">Update info</button>
+      <button @click="editPassword" class="btn btn-outline-warning btn-sm">
+        Update password
+      </button>
+      <button @click="editInfo" class="btn btn-outline-primary btn-sm">
+        Update info
+      </button>
     </div>
   </div>
 </template>
@@ -22,8 +32,8 @@ export default {
     },
     editInfo() {
       this.$emit("editInfo");
-    }
-  }
+    },
+  },
 };
 </script>
 
