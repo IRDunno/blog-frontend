c8308d9fd5a4db6c9f69dbc6dbf7e7eac28aaddc
aby-glophics <calagoaby.glophics@gmail.com>
2024-10-31 14:05:11 +0800

    fix: add total current entries out of all entries

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -23,7 +23,7 @@
           </a>
         </li>
       </ul>
-      <span>{{ meta.to }} out of {{ meta.total }} entries</span>
+      <span>{{ meta.to - 3 }}-{{ meta.to }} out of {{ meta.total }} entries</span>
     </nav>
   </div>
 </template>
