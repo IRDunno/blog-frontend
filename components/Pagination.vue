f58c4c697daaeb91c940ed21e6b438aa3a117c2a
aby-glophics <calagoaby.glophics@gmail.com>
2024-10-31 15:52:05 +0800

    trying to work with slice for number of pages

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -2,28 +2,62 @@
   <div>
     <nav class="d-flex justify-content-between align-items-center">
       <ul class="pagination mb-0">
+        <!-- PREVIOUS BUTTON START -->
         <li
-          v-for="(link, index) in meta.links"
+          :class="[
+            'page-item',
+            { active: meta.links[0].active },
+            { disabled: meta.links[0].url === null },
+          ]"
+        >
+          <a href="#" class="page-link" @click="loadMore(meta.links[0])">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        <!-- PREVIOUS BUTTON END -->
+
+        <!-- PAGINATION START -->
+        <li
+          v-for="(link, index) in meta.links.slice(paginationSlice)"
+          v-if="link.label !== `Next &raquo;`"
           :key="index"
           :class="['page-item', { active: link.active }]"
         >
           <a
             class="page-link"
             href="#"
-            aria-label="link.label"
+            :aria-label="link.label"
             @click="loadMore(link)"
           >
-            <span aria-hidden="true" v-if="link.label === '&laquo; Previous'"
-              >&laquo;</span
-            >
-            <span aria-hidden="true" v-else-if="link.label === 'Next &raquo;'"
-              >&raquo;</span
-            >
-            <span v-else>{{ link.label }}</span>
+            <span>{{ link.label }}</span>
           </a>
         </li>
+        <!-- PAGINATION END -->
+
+        <!-- NEXT BUTTON START -->
+        <li
+          :class="[
+            'page-item',
+            { active: meta.links[meta.last_page + 1].active },
+          ]"
+        >
+          <a
+            href="#"
+            :class="[
+              'page-link',
+              { disabled: meta.current_page === meta.last_page },
+            ]"
+            @click="loadMore(meta.links[meta.last_page + 1])"
+          >
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+        <!-- NEXT BUTTON END -->
       </ul>
-      <span>{{ meta.to - meta.per_page }}-{{ meta.to }} out of {{ meta.total }} entries</span>
+      <span
+        >{{ meta.to - meta.per_page }}-{{ meta.to }} out of
+        {{ meta.total }} entries</span
+      >
     </nav>
   </div>
 </template>
@@ -39,24 +73,34 @@ export default {
   data() {
     return {
       meta: {},
+      paginationSlice: 0,
     };
   },
   methods: {
     async loadMore(link) {
       // console.log(link.url);
-      if (link.label === 'Next &raquo;') link.url = this.meta.path + '?page=' + (this.meta.current_page + 1)
       if (link.url !== null) {
         const response = await this.$axios.$get(link.url);
         this.$emit("urlBlog", response.data);
         this.meta = response.meta;
-        console.log(this.meta)
-        console.log(response.meta.links);
+
+        if (this.meta.current_page >= 3) {
+          const setSlice = this.meta.current_page - 2;
+          console.log(setSlice);
+          this.paginationSlice = setSlice;
+          // console.log(this.meta.current_page);
+          // console.log(this.meta
+        }
+
+        console.log(this.meta);
+        // console.log(response.meta.links);
       }
     },
   },
-  mounted() {
+  created() {
     this.meta = this.metaLinks;
-    // console.log(this.metaLinks);
+    // this.paginationSlice = this.meta.current_page * this.
+    // console.log(this.meta.links[0]);
   },
 };
 </script>
